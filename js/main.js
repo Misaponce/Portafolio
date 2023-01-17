90b6aded1fbeaaeaf851b3e2d3b02565af45d39d
Misaponce <poncemisael@gmail.com>
2023-01-17 19:47:50 +0000

    added scroll effect to nav bar

--- js/main.js
+++ js/main.js
@@ -5,6 +5,30 @@ const navBar = document.querySelector('.nav');
 const menuNav = document.querySelector('.menu-nav');
 const navItems = document.querySelectorAll('.menu-nav-item');
 
+// variables for sticky navbar effect 
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset;
+
+    if (currentScroll <= 0 ) {
+        body.classList.remove("scroll-up")
+    }
+
+    if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+        body.classList.remove("scroll-up")
+        body.classList.add("scroll-down")
+    }
+
+    if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
+        body.classList.remove("scroll-down")
+        body.classList.add("scroll-up")
+    }
+
+    lastScroll = currentScroll;
+})
+
 // this will display the meny once the burguer is click
 let showMenu = false;
 
